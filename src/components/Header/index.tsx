f6d2bde8428c47c32154d843eb1904a49325d353
laripeanuts <larissarabelolf@gmail.com>
2022-04-16 03:28:36 -0300

    feat(componentes): implementa componente de switch
    Componetiza o switch do mui para adicionar um estilo do nosso tema
    
    #23

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,12 @@
-import { Switch } from "@mui/material";
-
-import { useTheme } from "../../contexts/useTheme";
-
 import { Container } from "./styles";
-
+import { SwitchComponent } from "../SwitchComponent";
 
 export const Header = () => {
-  const { theme, setTheme } = useTheme();
-
 
   return (
     <Container>
       <h1>Raro Academy Videos</h1>
-      <Switch
-        checked={theme.title === "dark"}
-        onChange={setTheme}
-        color="primary"
-      />
+      <SwitchComponent />
     </Container>
   );
 };
