3a1d4e2711ef5a9f776bedb944ed6d25f203cb43
laripeanuts <larissarabelf@gmail.com>
2022-04-21 18:21:56 -0300

    feat(context): aplica contexto global de autenticação no restando da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthProvider";
 import RoutesApp from "./routes";
 
 import GlobalStyles from "./styles/global";
 
 const App = () => (
-  <>
+  <AuthProvider>
     <BrowserRouter>
       <RoutesApp />
     </BrowserRouter>
     <GlobalStyles />
-  </>
+  </AuthProvider>
 );
 
 export default App;
