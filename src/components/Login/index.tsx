07caea1818dd8eac393f1f92a433d347d1008f75
laripeanuts <larissarabelf@gmail.com>
2022-04-21 18:22:54 -0300

    feat(context): aplica o contexto global na página de login
    n
    
    #39

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,51 +1,36 @@
-/* eslint-disable camelcase */
+import {
+  FormEvent, useContext, useEffect, useState,
+} from "react";
 import { Button, Input } from "@mui/material";
-import React, { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import apiClient from "../../services/api-client";
-
-interface userForm extends FormEvent<HTMLFormElement> {
-  children?: React.ReactNode | React.ReactNode[];
-}
+import { useAuth } from "../../contexts/AuthProvider/useAuth";
 
 export const Login = () => {
-  const navigate = useNavigate();
-
+  const auth = useAuth();
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setError("");
+  const handleSubmit = async (event: FormEvent) => {
     setLoading(true);
-
-    try {
-      const url = "/auth/login";
-      const response = await apiClient.post(url, { email, senha });
-      const { access_token, id } = response.data;
-      console.log(response);
-      if (access_token) {
-        localStorage.setItem("access_token", access_token);
-        localStorage.setItem("id", id);
-        navigate("/videos");
-      }
-    } catch (err: any) {
-      if (err.response.data.statusCode === 401) {
-        setError("Usuário ou senha Inválidos");
-      } else {
-        setError("Erro ao autenticar usuário. Tente novamente mais tarde.");
-      }
+    event.preventDefault();
+    await auth.authenticate(email, senha);
+    if (auth.error) {
+      setEmail("");
+      setSenha("");
     }
     setLoading(false);
   };
 
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate("/");
+  }, [auth.isAuthenticated, navigate]);
+
   return (
     <div>
-      <h2>Logar</h2>
-      <form onSubmit={handleLogin}>
-        <div style={{ display: "flex", flexDirection: "column" }}>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
           <Input
             type="text"
             name="email"
@@ -60,7 +45,10 @@ export const Login = () => {
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
           />
-          <div>{error && <div>{error}</div>}</div>
+          <div>
+            status:
+            {auth.error}
+          </div>
           <Button type="submit" disabled={loading}>
             {loading ? "Carregando..." : "Login"}
           </Button>
