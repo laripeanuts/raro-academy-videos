ebfdf90873c1ef7dfc081b4d7239d1868387f87e
Rogério Vieira <rogeriovbh@gmail.com>
2022-04-23 17:32:19 -0300

    feat(stayle): implementa estilização do footer
    Feita a estilização do footer com as dimenções providas pelo figma
    
    #49

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
-  
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 25px;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 80px;
+  position: fixed;
+  padding: 20px;
+
+  img{
+    height: 40px;
+    width: 40px;
+  }
+
 `;
