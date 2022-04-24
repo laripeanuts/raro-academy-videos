ea51768e522e73237a4b81f57127bd97094cbfe5
Thaynnah <thaynnah.cibelly@gmail.com>
2022-04-24 00:08:39 -0300

    feat: adiciona styles do link
    "re #30"

--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -2,5 +2,4 @@ import { Link, styled } from "@mui/material";
 
 export const StyledLink = styled(Link)(({ theme }) => ({
   color: theme.palette.primary.main,
-  border: theme.shape.borderRadius,
 }));
