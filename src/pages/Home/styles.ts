d4ef901185415c6796375cd9332c9756ea72eee3
laripeanuts <larissarabelf@gmail.com>
2022-04-23 22:21:57 -0300

    refactor(global): refatora alguns detalhes dos formuláios

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,5 @@ import { styled } from "@mui/material";
 
 export const Container = styled("section")`
   background: ${(props) => props.theme.palette.background.default};
+  color: ${(props) => props.theme.palette.text.primary};
 `;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,15 @@
 import { Login } from "../../components/Login";
+import { PassForgotten } from "../../components/PassForgotten";
+import { PassRecovery } from "../../components/PassRecovery";
 import { Container } from "./styles";
 
 export const LoginPage = () => (
   <Container className="login">
     <h1>Login</h1>
     <Login />
+    <h1>Esqueceu a senha</h1>
+    <PassForgotten />
+    <h1>Recupere a senha</h1>
+    <PassRecovery />
   </Container>
 );
