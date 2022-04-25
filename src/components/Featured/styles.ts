259f6f87baa22f38c8752848ec9095ecba8dac1d
laripeanuts <larissarabelf@gmail.com>
2022-04-25 04:06:37 -0300

    refactor(styles): ajusta detalhes de estilos para ficar de acordo com o figma
    #85

--- src/components/Featured/styles.ts
+++ src/components/Featured/styles.ts
@@ -2,9 +2,13 @@ import { styled } from "@mui/material/styles";
 
 export const Container = styled("section")`
   box-shadow: inset 1px 1px rgba(255, 255, 255, 0.3),
-    3px 3px 10px rgba(0, 0, 0, 0.4);
-  backdrop-filter: blur(20px);
-  background-color: rgba(255, 255, 255, 0.06);
+    3px 3px 10px rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(8px);
+  /* background-color: rgba(255, 255, 255, 0.02); */
   display: flex;
   border-radius: 10px;
+
+  background-color: ${(props) => (props.theme.palette.mode === "dark"
+    ? "rgba(87, 87, 87, 0.18)"
+    : "rgba(135, 135, 135, 0.13)")};
 `;
--- src/components/Link/styles.ts
+++ src/components/Link/styles.ts
@@ -2,4 +2,6 @@ import { Link, styled } from "@mui/material";
 
 export const StyledLink = styled(Link)(({ theme }) => ({
   color: theme.palette.text.primary,
+  fontSize: "0.8rem",
+  fontWeight: "bold",
 }));
