d2c84b867cbdd662b4c02cd3e9e0920ac2bbd396
laripeanuts <larissarabelf@gmail.com>
2022-04-24 22:16:36 -0300

    feat(rotas): acrescenta novas rotas de acordo com o layout do header
    #38

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,17 +4,20 @@ import { Home } from "../pages/Home";
 import { Layout } from "../pages/Layout";
 import { LoginPage } from "../pages/Login";
 import { NotFoundPage } from "../pages/NotFound";
+import { PassForgottenPage } from "../pages/PassForgotten";
+import { PassRecoveryPage } from "../pages/PassRecovery";
 import { RegisterPage } from "../pages/Register";
 import { VideoPage } from "../pages/Video";
 import { VideosPage } from "../pages/Videos";
 
 const RoutesApp = () => (
   <Routes>
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/register" element={<PassForgottenPage />} />
+      <Route path="/register" element={<PassRecoveryPage />} />
 
       <Route element={<RequireAuth />}>
         <Route path="/turmas/:turmaId/videos/:id" element={<VideoPage />} />
