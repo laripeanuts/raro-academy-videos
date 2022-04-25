edae9adb036bc04eef3acc7efd62283e7ef0955e
laripeanuts <larissarabelf@gmail.com>
2022-04-24 22:18:10 -0300

    feat(pages): implementa páginas de cadastro, novo usuário e esqueci minha senha
    #38

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,9 @@
 import { Login } from "../../components/Login";
-import { PassForgotten } from "../../components/PassForgotten";
-import { PassRecovery } from "../../components/PassRecovery";
 import { Container } from "./styles";
 
 export const LoginPage = () => (
   <Container className="login">
     <h1>Login</h1>
     <Login />
-    <h1>Esqueceu a senha</h1>
-    <PassForgotten />
-    <h1>Recupere a senha</h1>
-    <PassRecovery />
   </Container>
 );
---
+++ src/pages/PassForgotten/index.tsx
@@ -0,0 +1,9 @@
+import { PassForgotten } from "../../components/PassForgotten";
+import { Container } from "./styles";
+
+export const PassForgottenPage = () => (
+  <Container>
+    <h1>Esqueceu a senha</h1>
+    <PassForgotten />
+  </Container>
+);
