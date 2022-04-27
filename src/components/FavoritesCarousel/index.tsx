2a6b5a5ba885914d91b8b71490ec6b93937e15f8
Carlos Marinho <carlos.marinho20@outlook.com>
2022-04-27 20:38:46 -0300

    refactor: refatora componente Carousel
    Refatora componente FavoritesCarousel e componentiza o carousel de forma que possa ser utilizado por
    outros componentes.
    
    Closes #89

--- src/components/FavoritesCarousel/index.tsx
+++ src/components/FavoritesCarousel/index.tsx
@@ -1,44 +1,12 @@
-import { useEffect, useRef, useState } from "react";
 import { useVideos } from "../../hooks/useVideos";
-import { CarrouselButton } from "../CarrouselButton";
 import { FavoriteButton } from "../FavoriteButton";
 import { Featured } from "../Featured";
 import { Thumbnail } from "../Thumbnail";
-import { Carousel } from "./styles";
+import { Carousel } from "../Carousel";
 
 /* prettier-ignore */
 export const FavoritesCarousel = () => {
   const { favorites } = useVideos();
-  const carouselRef = useRef<HTMLDivElement>(null);
-  const [distFromLeft, setDistFromLeft] = useState(0);
-
-  const handleScroll = () => {
-    if (carouselRef.current) {
-      const distanceFromLeft = carouselRef.current.scrollLeft;
-
-      setDistFromLeft(distanceFromLeft);
-    }
-  };
-
-  const getHandleScrollClick = (offset: number) => () => {
-    if (carouselRef.current) {
-      carouselRef.current.scrollTo({ left: distFromLeft + offset, behavior: "smooth" });
-    }
-  };
-
-  useEffect(() => {
-    const carousel = carouselRef.current;
-    if (carousel) {
-      handleScroll();
-      carousel.addEventListener("scroll", handleScroll);
-    }
-
-    return () => {
-      if (carousel) {
-        carousel.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
 
   return (
     <Featured
@@ -49,12 +17,7 @@ export const FavoritesCarousel = () => {
         padding: "30px 15px",
       }}
     >
-      <CarrouselButton
-        left
-        aria-label="Item anterior"
-        onClick={getHandleScrollClick(-260)}
-      />
-      <Carousel ref={carouselRef}>
+      <Carousel itemsWidth={260}>
         {favorites.map((video) => (
           <Thumbnail
             videoId={video.id}
@@ -69,10 +32,6 @@ export const FavoritesCarousel = () => {
           </Thumbnail>
         ))}
       </Carousel>
-      <CarrouselButton
-        aria-label="Próximo item"
-        onClick={getHandleScrollClick(260)}
-      />
     </Featured>
   );
 };
