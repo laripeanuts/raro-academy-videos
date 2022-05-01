1f616f8085cd6da6860d1c79a5d3d42259169b3f
laripeanuts <larissarabelf@gmail.com>
2022-05-01 13:02:22 -0300

    feat(comments): adiciona scroll infinito a listagem de comentários
    #196

--- src/components/Comments/CommentList/index.tsx
+++ src/components/Comments/CommentList/index.tsx
@@ -1,20 +1,12 @@
 import { useEffect, useState, useRef } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
 
-import MapsUgcIcon from "@mui/icons-material/MapsUgc";
-import {
-  CircularProgress,
-  IconButton,
-  Tooltip,
-  Typography,
-} from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import { useComments } from "../../../hooks/useComments";
 
 import { CommentType } from "../../../types/CommentType";
 import { CommentItem } from "../CommentItem";
 
 import { Container } from "./styles";
-import Link from "../../Link";
 
 export const CommentList = () => {
   const { comments, loading, errorMessage } = useComments();
@@ -23,69 +15,71 @@ export const CommentList = () => {
   const [quantityMessage, setQuantityMessage] = useState(5);
   const [hasMore, setHasMore] = useState<boolean>();
 
-  const messagesEndRef = useRef<null | HTMLDivElement>(null);
+  const [isReverse, setIsReverse] = useState<boolean>(true);
+
+  const listEndRef = useRef<null | HTMLDivElement>(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    listEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   const loadMore = () => {
     if (comments.length > quantityMessage) {
       setHasMore(true);
-      setQuantityMessage(quantityMessage + 5);
+      setQuantityMessage(
+        (quantityMessageCurrent) => quantityMessageCurrent + 5,
+      );
     } else {
       setHasMore(false);
     }
   };
 
+  const loadLoading = () => {
+    if (loading) {
+      return <CircularProgress aria-label="Carregando conteúdo" />;
+    }
+
+    return null;
+  };
+
   /* prettier-ignore */
-  const loadCommentsList = () => (
-    loading ? (
-      <div>
-        {loading && (
-          <div className="progress">
-            <CircularProgress />
-            {!!errorMessage.length && errorMessage}
-          </div>
-        )}
+  const loadCommentsList = () => loading ? (
+    <div>
+      {loading && (
+      <div className="progress">
+        <CircularProgress />
+        {!!errorMessage.length && errorMessage}
       </div>
-    ) : (
-      <>
-        {hasMore ? (
-          <Tooltip title="Carregar mais mensagens" arrow>
-            <IconButton
-              onClick={() => loadMore()}
-              className="load-more-button"
-              disableRipple
-            >
-              <MapsUgcIcon />
-            </IconButton>
-          </Tooltip>
-        ) : null}
-        <ul>
-          {loadMessage?.map((item: CommentType) => (
-            <li key={item.id}>
-              <CommentItem
-                id={item.id}
-                texto={item.texto}
-                editado={item.editado}
-                createdAt={item.createdAt}
-                aluno={item.aluno}
-                upVotes={item.upVotes}
-                downVotes={item.downVotes}
-                meuVote={item.meuVote}
-              />
-            </li>
-          ))}
-        </ul>
-        <div ref={messagesEndRef} />
-      </>
-    ));
+      )}
+    </div>
+  ) : (
+    <>
+      <ul>
+        <li id="listEnd" />
+        {loadMessage?.map((item: CommentType) => (
+          <li key={item.id}>
+            <CommentItem
+              id={item.id}
+              texto={item.texto}
+              editado={item.editado}
+              createdAt={item.createdAt}
+              aluno={item.aluno}
+              upVotes={item.upVotes}
+              downVotes={item.downVotes}
+              meuVote={item.meuVote}
+            />
+          </li>
+        ))}
+      </ul>
+      <div ref={listEndRef} />
+    </>
+  );
 
   useEffect(() => {
-    console.log("quantidade de mensagens", comments.length);
-    console.log("quantidade de quant mensagens", quantityMessage);
-    console.log(hasMore, "hasMore");
+    scrollToBottom();
+  }, [comments]);
+
+  useEffect(() => {
     setLoadMessage([...comments].splice(-quantityMessage, quantityMessage));
   }, [comments, quantityMessage]);
 
@@ -94,7 +88,24 @@ export const CommentList = () => {
   }, [comments]);
 
   useEffect(() => {
-    scrollToBottom();
+    const listEndReference: Element = document.querySelector(
+      "#listEnd",
+    ) as HTMLElement;
+
+    if (listEndReference) {
+      const intersectionObserver = new IntersectionObserver((entries) => {
+        setTimeout(() => {
+          if (entries.some((entry) => entry.isIntersecting)) {
+            loadMore();
+          }
+        }, 1000);
+      });
+      intersectionObserver.observe(listEndReference);
+
+      return () => intersectionObserver.disconnect();
+    }
+
+    return () => {};
   }, [comments]);
 
   return (
