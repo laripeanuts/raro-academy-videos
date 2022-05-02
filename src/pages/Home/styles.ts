40b9c8e2eb6ca5c69f909c048561556f2a3c14d4
laripeanuts <larissarabelf@gmail.com>
2022-05-02 16:47:14 -0300

    refactor(components): retira componentes não mais utilizados
    #241

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 
 export const Container = styled("section")`
+  padding-top: 20px 0;
   display: flex;
   width: 100%;
   flex-direction: column;
@@ -13,16 +14,16 @@ export const LinkText = styled(Typography)`
   padding: 10px;
   border-radius: 10px;
   font-weight: bold;
-  color: ${({ theme }) => theme.palette.primary.main};
+  opacity: 0.7;
+  color: ${({ theme }) => theme.palette.text.primary};
 
-  & :hover {
-    
+  :hover {
+    color: ${({ theme }) => theme.palette.primary.main};
+    opacity: 1;
     backdrop-filter: blur(8px);
-    background-color: ${(props) => (
-    props.theme.palette.mode === "dark"
-      ? "rgba(87, 87, 87, 0.18)"
-      : "rgba(135, 135, 135, 0.13)"
-  )};
+    background-color: ${(props) => props.theme.palette.mode === "dark"
+    ? "rgba(87, 87, 87, 0.18)"
+    : "rgba(135, 135, 135, 0.13)"};
   }
 `;
 
