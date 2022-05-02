b23452b05ce6af6f41932014b47ec2be79293677
laripeanuts <larissarabelf@gmail.com>
2022-05-02 16:29:59 -0300

    style(responsividade): melhora responsividade da tela de player
    #238

--- src/components/Comments/CommentList/styles.ts
+++ src/components/Comments/CommentList/styles.ts
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 export const Container = styled("section")`
   padding-top: 15px;
   width: 100%;
-  height: 90%;
+  height: 88%;
   display: flex;
   flex-direction: column-reverse;
   justify-content: center;
